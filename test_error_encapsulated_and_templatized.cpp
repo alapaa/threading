8b08ccdc6ea0d768a7679bac1689e65bbe1acc96
Erik Alapää <ealapaa@nvidia.com>
2024-01-09 11:22:59 +0100

    Name changes, polishing.

--- test_error_encapsulated_and_templatized.cpp
+++ test_error_encapsulated_and_templatized.cpp
@@ -2,10 +2,15 @@
 #include <string>
 #include <variant>
 
+// Inspired by absl::StatusOr. Return a result or an error message.
+// See https://abseil.io/docs/cpp/guides/status.
+
+namespace cloud {
+namespace utils {
 using ErrMsg = std::string;
 
 template <typename T, typename Base = std::variant<T, ErrMsg>>
-class MyStatus : public Base {
+class StatusOr : private Base {
  public:
   using Base::Base;
   bool ok() const { return std::holds_alternative<T>(*this); }
@@ -14,16 +19,22 @@ class MyStatus : public Base {
   const ErrMsg& error() const { return std::get<ErrMsg>(*this); }
 };
 
-MyStatus<int> do_something(int arg) {
+
+
+// Usage example:
+//
+}
+}
+
+cloud::utils::StatusOr<int> do_something(int arg) {
   if (arg == 42) return arg*2;
-  else return ErrMsg{"Wrong arg! Cannot compute result."};
+  else return cloud::utils::ErrMsg{"Wrong arg! Cannot compute result."};
 }
 
 int main() {
   auto result = do_something(42);
   if (result.fail()) std::cout << result.error() << '\n';
   else std::cout << "Success! Result is " << result.value() << '\n';
-
   result = do_something(43);
   if (result.fail()) std::cout << result.error() << '\n';
   else std::cout << "Success! Result is" << result.value() << '\n';
