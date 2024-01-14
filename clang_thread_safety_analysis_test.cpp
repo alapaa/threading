042ad883937b34229b35f1c4a0ce8414fa931a87
Erik Alapää <ealapaa@nvidia.com>
2024-01-14 15:10:30 +0100

    First working test of Clang thread safety analysis with a simple std::mutex wrapper.

---
+++ clang_thread_safety_analysis_test.cpp
@@ -0,0 +1,24 @@
+#include "thread_safety_mutex.h"
+
+// May need
+// sudo apt install libc++-dev
+// clang++ -stdlib=libc++ <rest of arguments>
+// and possibly static linking of libc++abi.
+//#include "clanghack.h"
+
+class Foo {
+public:
+  void set_val(int val) {
+    MutexLocker lg(&mu_);
+    val_ = val;
+  }
+private:
+  Mutex mu_;
+  int val_ GUARDED_BY(mu_);
+  //int val_ THREAD_ANNOTATION_ATTRIBUTE__(guarded_by(mu_));
+};
+
+int main() {
+  Foo foo;
+  foo.set_val(5);
+}
